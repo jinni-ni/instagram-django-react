a19654c67dac8af1627e536cd2051c953638896b
김수진 <sjkim910115@gmail.com>
2020-11-20 22:28:29 +0900

    comment push

---
+++ frontend/src/components/Comment.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Avatar, Tooltip, Comment as AntdComment } from "antd";
+import moment from "moment";
+
+export default function Comment({ comment }) {
+  const {
+    author: { username, name, avatar_url },
+    message,
+    created_at,
+  } = comment;
+  const displayName = name.length === 0 ? username : name;
+  return (
+    <div>
+      <AntdComment
+        author={displayName}
+        avatar={
+          // FIXME : avatar url host
+          <Avatar
+            src={"http://localhost:8000" + avatar_url}
+            alt={displayName}
+          />
+        }
+        content={<p>{message}</p>}
+        datetime={
+          <Tooltip title={moment().format(created_at)}>
+            <span>{moment(created_at).fromNow()}</span>
+          </Tooltip>
+        }
+      />
+    </div>
+  );
+}
